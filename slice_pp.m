405991b5be53bc2b045d044caff1758023fe4f0f
Stephen P. Cook <s.cook@bath.ac.uk>
2015-10-31 09:59:31 +0000

    Provisional routine for slice interpolation

--- slice_pp.m
+++ slice_pp.m
@@ -16,7 +16,7 @@ function [rho_pp, rho_int_pp] = slice_pp(z_A, zh_A, rhobar)
 
 global N
 %=======
-% INPUT 
+% INPUT
 %=======
 % z_A - The points defining the eularian control volumes (ECV).
 % zh_A - Currently not used; assumed to be the midpoints of z_A.
@@ -42,7 +42,7 @@ Dz_A = diff(z_A);
 
 % rho_hat
 %---------
-% First we want to build up the N-3 interval pp rhohat(z) with 
+% First we want to build up the N-3 interval pp rhohat(z) with
 %   \int_{I_j} rhohat(z) = rhobar_j
 % with j \in \{i-2, i-1, i, i+1\}.
 %
@@ -50,9 +50,9 @@ Dz_A = diff(z_A);
 C1 = z_A(1:end-1) + z_A(2:end);
 C2 = z_A(1:end-1).^2 + z_A(2:end).^2;
 C3 = z_A(1:end-1).*z_A(2:end);
-C1 = C1(:); 
+C1 = C1(:);
 C2 = C2(:);
-C3 = C3(:); 
+C3 = C3(:);
 
 % We're going to move through this pulling out a 4-by-4 matrix from hugeA
 % for each interval. Calculated from analytical integrals required to meet
@@ -62,9 +62,9 @@ hugeA = [C1.*C2./4, (C2+C3)./3, C1./2, ones(N,1)];
 % Local coefficients of the rho_hat pp's.
 %-----------------------------------------
 for ii = 1:N-3; % Create all the coefficients for the pp of rhohat.
-  
+
   hat_coefs(ii,1:4) = (hugeA(ii:ii+3, 1:4)\rhobar(ii:ii+3)')';
-  
+
   % What a horrible mess. So the coefficients I found were for a regular
   % polynomial, like
   %   if z_L < z < z_R
@@ -75,7 +75,7 @@ for ii = 1:N-3; % Create all the coefficients for the pp of rhohat.
   %     pp1(z) = a1*(z-z_L)^3 + a2*(z-z_L)^2 + a3*(z-z_L) + a4
   %   end if
   % Instead of going back to derive the a#'s corresponding to this
-  % formulation, I'm just going to do a global to local transformation. 
+  % formulation, I'm just going to do a global to local transformation.
   %  I'll call this an acceptable intellectual overhead.
   zi = z_A(ii+2);
   local_change = [...
@@ -83,7 +83,7 @@ for ii = 1:N-3; % Create all the coefficients for the pp of rhohat.
     3*zi, 1, 0, 0;...
     3*zi^2, 2*zi, 1, 0;...
     zi^3, zi^2, zi, 1];
-  
+
   % Row vector, so right hand multiplication
   hat_coefs(ii,1:4) = hat_coefs(ii,1:4)*local_change';
 end
--- slice_pp2.m
+++ slice_pp2.m
@@ -1,4 +1,4 @@
-% function [rho_pp, rho_int_pp] = slice_pp2(z_A, zh_A, rhobar)
+function [rho_pp, rho_int_pp] = slice_pp2(z_A, zh_A, rhobar)
 
 % global N
 N = 60;
@@ -37,3 +37,49 @@ intervals = N-4;
 
 masses = Dz_A.*rhobar;
 % coefs of a lagrangian polynomial?
+
+% We need to evaluate rho_L and rho_R (derivative of cumulative masses) at
+% all x_i's and at all midpoints, and rho_L_prime and rho_R_prime (second
+% derivative of cumulative masses) at all the midpoints.
+
+%%%%%%%%%%%%%
+% MAIN LOOP %
+%%%%%%%%%%%%%
+
+for ii = 2:(n-2)
+  % All assuming indexing starts at zero for both X and masses
+  X_local = X(ii-3):X(ii+1);
+  Y_local = [0; cumsum(masses(ii-2:ii+1))];
+  X_halfs = [(X(ii-2)+X(ii-1))/2, (X(ii-1)+X(ii))/2];
+
+  rho_L(ii) = diff_lagrange(X_local, Y_local, X(ii-1))
+  [rho_L_prime_m(ii), rho_L_prime_p(ii)] = ...
+    ddiff_quartic_lagrange(X_local,Y_local,X_halfs);
+end % for ii
+% This should be the code for calculating the defining points
+rho_i_minus = rho_L(i-1);
+rho_i = rho_L(i);
+rho_i_half = 3*masses(i)/(2*Dxi(i)) - 0.25*(rho_i_minus + rho_i);
+rho_i_plus = calc_rho_i_plus_one(rho_i_minus, rho_i_half, rho_i,...
+  rho_L_prime_p(i), rho_L_prime_m(i+1), Dxi, Dxi_plus);
+% These above should be calculated once then output (quite expensive), and
+% then they can be used many times (relatively cheap).
+%
+%rho_D = my_lagrange(%[X(i-1), 0.5*(X(i-1)+X(i)), X(i), X(i+1)],...
+%                    %[rho_i_minus, rho_i_half, rho_i, rho_i_plus]...
+%                    %,X_out)
+
+function p_three = calc_rho_i_plus_one(rho_i_minus, rho_i_half, rho_i,...
+                                d_rho_L_half, d_rho_R_half, Dxi, Dxi_plus)
+  % Calculates p_three as in thesis/slice.tex equation eq:slice:pThree
+  pi_prime_i_m = (0.5*Dxi + Dxi_plus)/(Dxi*(Dxi + Dxi_plus));
+  pi_prime_i_half = 1/(0.5*Dxi + Dxi_plus);
+  pi_prime_i = (0.5*Dxi + Dxi_plus)/(Dxi*Dxi * plus);
+  pi_prime_i_plus = 1/4*(Dxi^2)/...
+    ((Dxi + Dxi_plus)*(0.5*Dxi+Dxi_plus)*Dxi_plus);
+
+  p_three = 1/pi_prime_i_plus * (0.5*(d_rho_L_half + d_rho_R_half) - ...
+    rho_i_minus*pi_prime_i_m - rho_i_half*pi_prime_i_half - ...
+  rho_i*pi_prime_i);
+end % function p_three
+end % function slice_pp2
